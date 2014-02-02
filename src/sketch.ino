dcca33f34e922d3513ee472fef3a72aa4d704fd7
Alex Beard <bearda@purdue.edu>
2014-02-01 21:46:00 -0500

    Reads whole byte

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,13 +1,27 @@
 
 #define LED_PIN 13
-#define INPUT_PIN 2
+#define INPUT_PIN_0 2
+#define INPUT_PIN_1 3
+#define INPUT_PIN_2 4
+#define INPUT_PIN_3 5
+#define INPUT_PIN_4 6
+#define INPUT_PIN_5 7
+#define INPUT_PIN_6 8
+#define INPUT_PIN_7 9
 
 void setup()
 {
     //I just made a change
     pinMode(LED_PIN, OUTPUT);
     Serial.begin(9600);
-    pinMode(INPUT_PIN,INPUT);
+    pinMode(INPUT_PIN_0,INPUT);
+    pinMode(INPUT_PIN_1,INPUT);
+    pinMode(INPUT_PIN_2,INPUT);
+    pinMode(INPUT_PIN_3,INPUT);
+    pinMode(INPUT_PIN_4,INPUT);
+    pinMode(INPUT_PIN_5,INPUT);
+    pinMode(INPUT_PIN_6,INPUT);
+    pinMode(INPUT_PIN_7,INPUT);
 }
 
 void loop()
@@ -16,6 +30,25 @@ void loop()
     delay(100);
     digitalWrite(LED_PIN, LOW);
     delay(900);
-    int buttonState = digitalRead(INPUT_PIN);
-    Serial.println(buttonState);
+    String byte;
+    char nextChar;
+    //read all the pins, in order
+    nextChar = char(digitalRead(INPUT_PIN_0));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_1));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_2));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_3));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_4));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_5));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_6));
+    byte = byte + nextChar;
+    nextChar = char(digitalRead(INPUT_PIN_7));
+    byte = byte + nextChar;
+    //print line to serial port
+    Serial.println(byte);
 }
