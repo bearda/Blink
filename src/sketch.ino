9e03dff617367d51bd2436dc62bd6fc96bcf139a
Alex Beard <bearda@purdue.edu>
2014-02-02 00:39:33 -0500

    Reads bit from serial, Writes bit to LEDs

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,54 +1,55 @@
 
 #define LED_PIN 13
-#define INPUT_PIN_0 2
-#define INPUT_PIN_1 3
-#define INPUT_PIN_2 4
-#define INPUT_PIN_3 5
-#define INPUT_PIN_4 6
-#define INPUT_PIN_5 7
-#define INPUT_PIN_6 8
-#define INPUT_PIN_7 9
+#define OUTPUT_PIN_0 2
+#define OUTPUT_PIN_1 3
+#define OUTPUT_PIN_2 4
+#define OUTPUT_PIN_3 5
+#define OUTPUT_PIN_4 6
+#define OUTPUT_PIN_5 7
+#define OUTPUT_PIN_6 8
+#define OUTPUT_PIN_7 9
+
+String readString = "";
+char character;
 
 void setup()
 {
     //I just made a change
     pinMode(LED_PIN, OUTPUT);
     Serial.begin(9600);
-    pinMode(INPUT_PIN_0,INPUT);
-    pinMode(INPUT_PIN_1,INPUT);
-    pinMode(INPUT_PIN_2,INPUT);
-    pinMode(INPUT_PIN_3,INPUT);
-    pinMode(INPUT_PIN_4,INPUT);
-    pinMode(INPUT_PIN_5,INPUT);
-    pinMode(INPUT_PIN_6,INPUT);
-    pinMode(INPUT_PIN_7,INPUT);
+    pinMode(OUTPUT_PIN_0,OUTPUT);
+    pinMode(OUTPUT_PIN_1,OUTPUT);
+    pinMode(OUTPUT_PIN_2,OUTPUT);
+    pinMode(OUTPUT_PIN_3,OUTPUT);
+    pinMode(OUTPUT_PIN_4,OUTPUT);
+    pinMode(OUTPUT_PIN_5,OUTPUT);
+    pinMode(OUTPUT_PIN_6,OUTPUT);
+    pinMode(OUTPUT_PIN_7,OUTPUT);
+    Serial.println("Please write a byte");
 }
 
 void loop()
 {
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-    digitalWrite(LED_PIN, LOW);
-    delay(900);
-    String byte = "";
-    int nextChar;
-    //read all the pins, in order
-    nextChar = digitalRead(INPUT_PIN_0);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_1);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_2);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_3);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_4);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_5);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_6);
-    byte = byte + nextChar;
-    nextChar = digitalRead(INPUT_PIN_7);
-    byte = byte + nextChar;
+    while (Serial.available() > 0) {
+        delay(5);
+        character = Serial.read();
+        readString += character;
+        Serial.print(character);
+    }
+    //wait for the buffer to grow
+    if (readString.length() >= 8) {
+        //read all the pins, in order
+        digitalWrite(OUTPUT_PIN_0,int(readString[0] - 48));
+        digitalWrite(OUTPUT_PIN_1,int(readString[1] - 48));
+        digitalWrite(OUTPUT_PIN_2,int(readString[2] - 48));
+        digitalWrite(OUTPUT_PIN_3,int(readString[3] - 48));
+        digitalWrite(OUTPUT_PIN_4,int(readString[4] - 48));
+        digitalWrite(OUTPUT_PIN_5,int(readString[5] - 48));
+        digitalWrite(OUTPUT_PIN_6,int(readString[6] - 48));
+        digitalWrite(OUTPUT_PIN_7,int(readString[7] - 48));
+        Serial.println("  Input received!");
+        Serial.println("Please write a byte");
+        readString = "";
+    }
     //print line to serial port
-    Serial.println(byte);
 }
